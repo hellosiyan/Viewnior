97102a7f1953f5206a5dbda620656298c0c77fac
Siyan Panayotov <xsisqox@gmail.com>
2009-05-24 16:17:49 +0300

    Fix error when dragging items from GnomeMenu
    Fix crash, when dragging item from the Place menu.

--- src/vnr-tools.c
+++ src/vnr-tools.c
@@ -90,28 +90,23 @@ vnr_tools_get_list_from_array (gchar **files)
     return g_slist_reverse (uri_list);
 }
 
+/* modified version of eog's
+ * eog_util_parse_uri_string_list_to_file_list */
 GSList*
 vnr_tools_parse_uri_string_list_to_file_list (const gchar *uri_list)
 {
     GSList* file_list = NULL;
-    const char *start, *end;
-    char *uri;
+    gsize i = 0;
+    gchar **uris;
 
-    start = uri_list;
-    end = strchr (uri_list, '\r');
-    uri = g_strndup (start, end - start);
-    file_list = g_slist_prepend (file_list, g_file_get_path (g_file_new_for_uri (uri)));
-    g_free (uri);
+    uris = g_uri_list_extract_uris (uri_list);
 
-    while (strlen (end) > 2)
-    {
-        start = (end + 2);
-        end = strchr (start, '\r');
-        uri = g_strndup (start, end - start);
-        file_list = g_slist_prepend (file_list, g_file_get_path (g_file_new_for_uri (uri)));
-        g_free (uri);
+    while (uris[i] != NULL) {
+        file_list = g_slist_append (file_list,g_file_get_path (g_file_new_for_uri(uris[i])));
+        i++;
     }
 
+    g_strfreev (uris);
     return g_slist_reverse (file_list);
 }
 
