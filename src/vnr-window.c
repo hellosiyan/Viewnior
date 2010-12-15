e818ca1422e9c6a1041e165f01ec86e4816bb529
Siyan Panayotov <xsisqox@gmail.com>
2010-12-15 20:21:47 +0200

    Viewnior 1.1 released!

--- src/vnr-window.c
+++ src/vnr-window.c
@@ -1219,7 +1219,7 @@ vnr_window_cmd_about (GtkAction *action, VnrWindow *window)
     gtk_show_about_dialog (GTK_WINDOW (window),
                    "program-name", "Viewnior",
                    "version", VERSION,
-                   "copyright", "Copyright \xc2\xa9 2009 Siyan Panayotov <xsisqox@gmail.com>",
+                   "copyright", "Copyright \xc2\xa9 2009-2010 Siyan Panayotov <xsisqox@gmail.com>",
                    "comments",_("Elegant Image Viewer"),
                    "authors", authors,
                    "logo-icon-name", "viewnior",
