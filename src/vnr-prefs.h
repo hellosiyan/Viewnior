5d1c9bde6d460f1327d9c0aedb8648cb52f68d5f
Maarten de Vries <maarten@de-vri.es>
2017-12-24 17:04:08 +0100

    Add option to hide the scrollbars.

--- src/vnr-prefs.h
+++ src/vnr-prefs.h
@@ -98,6 +98,7 @@ struct _VnrPrefs {
     gboolean reload_on_save;
     gboolean show_menu_bar;
     gboolean show_toolbar;
+    gboolean show_scrollbar;
     gboolean start_maximized;
     gboolean start_slideshow;
     gboolean start_fullscreen;
@@ -121,8 +122,9 @@ GType     vnr_prefs_get_type (void) G_GNUC_CONST;
 GObject*  vnr_prefs_new (GtkWidget *window);
 void      vnr_prefs_show_dialog (VnrPrefs *prefs);
 void      vnr_prefs_set_slideshow_timeout   (VnrPrefs *prefs, int value);
-void      vnr_prefs_set_show_toolbar    (VnrPrefs *prefs, gboolean show_toolbar);
 void      vnr_prefs_set_show_menu_bar   (VnrPrefs *prefs, gboolean show_menu_bar);
+void      vnr_prefs_set_show_toolbar    (VnrPrefs *prefs, gboolean show_toolbar);
+void      vnr_prefs_set_show_scrollbar    (VnrPrefs *prefs, gboolean show_scollbar);
 gboolean  vnr_prefs_save (VnrPrefs *prefs);
 
 G_END_DECLS
